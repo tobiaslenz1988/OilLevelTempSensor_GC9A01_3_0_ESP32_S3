737ba031d1a7f5d908ffb93099d729eb561e48a6
tobiaslenz1988 <tobias.lenz88@live.de>
2026-05-20 02:55:08 +0200

    Add files via upload

---
+++ display/Oil_LevelGraphic_GCA901_240_240/oil_level_graphic.h
@@ -0,0 +1,17 @@
+#ifndef OIL_LEVEL_GRAPHIC_H
+#define OIL_LEVEL_GRAPHIC_H
+#include "_0percent_inv8.h"
+#include "_10percent_inv8.h"
+#include "_20percent_inv8.h"
+#include "_30percent_inv8.h"
+#include "_40percent_inv8.h"
+#include "_50percent_inv8.h"
+#include "_60percent_inv8.h"
+#include "_70percent_inv8.h"
+#include "_80percent_inv8.h"
+#include "_90percent_inv8.h"
+#include "_100percent_inv8.h"
+
+
+#endif  /* OIL_LEVEL_GRAPHIC_H */
+
---
+++ sensors/display_selector.h
@@ -0,0 +1,9 @@
+#ifndef SENSORS_H
+#define SENSORS_H
+ 
+#include "watertempsensor.h"
+#include "oiltempsensor.h"
+#include "boostpressure.h"
+
+
+#endif /* SENSORS_H */
