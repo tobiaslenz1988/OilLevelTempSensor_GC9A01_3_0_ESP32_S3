dbdd083e4c02bdad9bf072c9833c67d14108f955
tobiaslenz1988 <tobias.lenz88@live.de>
2026-05-23 21:51:05 +0200

    corrected sw version
    corrected sw version

--- softwareversion.h
+++ softwareversion.h
@@ -1,5 +1,12 @@
 #ifndef SOFTWAREVERSION_H
 #define SOFTWAREVERSION_H
 
-#define SOFTWAREVERSION               "Y00A"
+
+//                                     1234
+//                                     1) A= SSD1306  B= GC9A01
+//                                     2) A= ESP32    B= ESP32 S3
+//                                     3) + 4) SW Version 16Bit
+#define SOFTWAREVERSION               "BB0C"
+
+
 #endif /* SOFTWAREVERSION_H */
